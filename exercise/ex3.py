31879ea4cff4d692740bd305e1b1997212ba5247
toche <t.samanchuen.gmail.com>
2024-06-29 05:22:02 +0700

    complete exercise

--- exercise/ex3.py
+++ exercise/ex3.py
@@ -7,32 +7,16 @@ def homework():
     exDf
 
     # create model 1 for linear regression
-
     from sklearn.linear_model import LinearRegression
-    linreg = LinearRegression()
-    X = exDf[['Temp']]
-    y = exDf['Yield']
-    linreg.fit(X, y)
-    yhat = linreg.predict(X)
-    scoreModel1 = linreg.score(X, y)
-    
-    # plt.plot(X, y, 'o')
-    # plt.plot(X, yhat, '-')
-    # plt.show()
+ 
 
 
     # create model 2 for linear regression wih polynomial features
     from sklearn.preprocessing import PolynomialFeatures
-    poly = PolynomialFeatures(degree=2)
-    X_poly = poly.fit_transform(X)
-    linreg.fit(X_poly, y)
-    yhat = linreg.predict(X_poly)
-    scoreModel2 = linreg.score(X_poly, y)
-    #print(scoreModel2)
-    # plt.plot(X, y, 'o')
-    # plt.plot(X, yhat, '-')
-    # plt.show()
-
+ 
+    # return the value of the score for model 1 and model 2
+    scoreModel1 = None
+    scoreModel2 = None
     return scoreModel1, scoreModel2
 
 
